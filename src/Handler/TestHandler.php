f4d20eb17cc174ea0656d236995f6bd9f503ea58
platypus <santa@claus>
2021-08-03 15:50:42 +0200

    Add test on link

--- src/Handler/TestHandler.php
+++ src/Handler/TestHandler.php
@@ -5,13 +5,8 @@ namespace Micorx\Welper\Handler;
 class TestHandler
 {
 
-	function e_test_youtube_video($link, $is_full_link)
+	private function s_test_link($url)
 	{
-		if ($is_full_link === true) {
-			$url = 'https://www.youtube.com/oembed?format=json&url=' . $link;
-		} else {
-			$url = 'https://www.youtube.com/oembed?format=json&url=www.youtube.com/watch?v=' . $link;
-		}
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 500);
@@ -19,10 +14,34 @@ class TestHandler
 		curl_exec($ch);
 		$returned_status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
+		return $returned_status_code;
+	}
+
+	function e_test_link($link, $http_code)
+	{
+		if (str_starts_with($this->s_test_link($link), $http_code)) {
+			return $link;
+		} else {
+			return false;
+		}
+	}
+
+	function e_test_youtube_video($video, $is_full_link)
+	{
+		if ($is_full_link === true) {
+			$link = 'https://www.youtube.com/oembed?format=json&url=' . $video;
+		} else {
+			$link = 'https://www.youtube.com/oembed?format=json&url=www.youtube.com/watch?v=' . $video;
+		}
+		$returned_status_code = $this->s_test_link($link);
 		if (substr($returned_status_code, 0, 1) == '4' || substr($returned_status_code, 0, 1) == '5') {
 			return false;
 		} else {
-			return true;
+			if ($is_full_link === true) {
+				return $video;
+			} else {
+				return 'https://www.youtube.com/watch?v=' . $video;
+			}
 		}
 	}
 }
--- src/Test.php
+++ src/Test.php
@@ -18,19 +18,45 @@ class Test
 		}
 	}
 
+	function test_link($link, $http_code = 200)
+	{
+		/**
+		 *
+		 * @param string $link
+		 *        	Link to test
+		 * 
+		 * @param string $http_code
+		 *        	HTTP code to get, it is allowed the initial digits of the code
+		 * 
+		 * @return $link: if the link returns the http code requested
+		 *         false: if the link NOT returns the http code requested
+		 *
+		 */
+		$this->start_engine();
+		if ($this->engine->e_test_link($link, $http_code) !== false) {
+			return $link;
+		} else {
+			return false;
+		}
+	}
+
 	function test_youtube_video($link, $is_full_link = false)
 	{
 		/**
 		 *
 		 * @param string $link
-		 *        	Link of YouTube video
+		 *        	Link or ID of YouTube video
+		 * @param string $is_full_link
+		 *        	true if it is the full link
+		 *        	false if it contains just the video ID
 		 *
 		 * @return $link: if the video exist
 		 *         false: if is not possible to determine if the video exists
 		 *
 		 */
 		$this->start_engine();
-		if ($this->engine->e_test_youtube_video($link, $is_full_link) !== false) {
+		$link = $this->engine->e_test_youtube_video($link, $is_full_link);
+		if ($link !== false) {
 			return $link;
 		} else {
 			return false;
